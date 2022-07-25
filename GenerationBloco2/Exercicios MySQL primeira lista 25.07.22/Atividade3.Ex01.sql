d27685af7e449b05021d34b12e2684488f20dd01
Fernando <fernandohemmeldossantos@gmail.com>
2022-07-25 13:32:44 -0300

    Commit para envio da atividade 3 exercico 01 da primeira lista de MySQL 25.07.22

---
+++ GenerationBloco2/Exercicios MySQL primeira lista 25.07.22/Atividade3.Ex01.sql
@@ -0,0 +1,6 @@
+/* Atividade 3  Exercicio 01 - Crie um banco de dados para um registro de uma escola, onde o sistema trabalhará
+com as informações dos estudantes deste registro dessa escola.. */
+
+CREATE DATABASE BD_ESCOLA;
+
+USE BD_ESCOLA;
