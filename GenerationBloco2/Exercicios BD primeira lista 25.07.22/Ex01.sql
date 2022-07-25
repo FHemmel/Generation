21cd55d5e12d5a53122cdfd54a1d0ea32c772d51
Fernando <fernandohemmeldossantos@gmail.com>
2022-07-25 11:25:15 -0300

    Commit para envio do exercico 02 da primeira lista de BD 25.07.22

--- GenerationBloco2/Exercicios BD primeira lista 25.07.22/Ex01.sql
+++ GenerationBloco2/Exercicios BD primeira lista 25.07.22/Ex01.sql
@@ -1,4 +1,6 @@
 /* Exercicio 01 - Crie um banco de dados para um serviço de RH de uma empresa, onde o sistema trabalhará com as 
 informações dos colaboradores desta empresa */
 
-CREATE DATABASE BD_RH
+CREATE DATABASE BD_RH;
+
+USE BD_RH;
---
+++ GenerationBloco2/Exercicios BD primeira lista 25.07.22/Ex02.sql
@@ -0,0 +1,17 @@
+/* Exercicio 02 - Crie uma tabela de colaboradores e determine 5 atributos relevantes dos colaboradores para se trabalhar
+ com o serviço deste RH. */
+
+CREATE DATABASE BD_RH;
+
+USE BD_RH;
+
+CREATE TABLE COLABORADORES(
+ID BIGINT AUTO_INCREMENT,
+NOME VARCHAR(255),
+CPF CHAR(11),
+IDADE INT,
+SALARIO DOUBLE,
+CARGO CHAR(255),
+primary key(ID)
+);
+
